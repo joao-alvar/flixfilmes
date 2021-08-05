eac21123d4b3dabbb991dfd239c711c9bac33729
João Alvarenga <jvalvarenga@mailfence.com>
2021-08-04 23:03:18 -0300

    add updates

--- main.js
+++ main.js
@@ -2,35 +2,36 @@ const main = document.querySelector("main");
 const form = document.querySelector("#form");
 const search = document.querySelector("#search");
 
-getFilmes();
+getFilmes(APIURL);
 
-async function getFilmes() {
-  const resp = await fetch(APIURL);
+async function getFilmes(url) {
+  const resp = await fetch(url);
   const respData = await resp.json();
 
-  console.log(respData);
+  mostrarFilmes(respData.results);
+}
 
-  respData.results.forEach((movie) => {
+function mostrarFilmes(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
     const { poster_path, title, vote_average } = movie;
 
     const filmeEl = document.createElement("div");
     filmeEl.classList.add("filmes");
 
     filmeEl.innerHTML = `
-      <img
-        src="${POSTERPATH + poster_path}"
-        alt="${title}"
-      />
-      <div class="filmes-info">
-        <h3 class="filme-titulos">${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-      </div>
-      `;
+          <img
+            src="${POSTERPATH + poster_path}"
+            alt="${title}"
+          />
+          <div class="filmes-info">
+            <h3 class="filme-titulos">${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          </div>
+          `;
 
     main.appendChild(filmeEl);
   });
-
-  return respData;
 }
 
 function getClassByRate(vote) {
@@ -47,4 +48,10 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const searchTerm = search.value;
+
+  if (searchTerm) {
+    getFilmes(SEARCHAPI + searchTerm);
+
+    search.value = "";
+  }
 });
