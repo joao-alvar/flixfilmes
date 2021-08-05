8ab0e25e90a1553e18ef1a3418e9a355519ab4c9
João Alvarenga <jvalvarenga@mailfence.com>
2021-08-05 18:12:00 -0300

    add updates

---
+++ api.js
@@ -0,0 +1,6 @@
+const APIKEY = "04c35731a5ee918f014970082a0088b1";
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const POSTERPATH = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query=";
